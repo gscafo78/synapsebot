f481d3e028a260e28b36c42e3dae2727e1ed4ec4
Giovanni Scafetta <giovanni.scafetta@gmail.com>
2025-03-13 15:37:06 +0100

    fix wellcome

--- rssbot.py
+++ rssbot.py
@@ -188,11 +188,10 @@ class RSSBot:
                                 if event["type"] == "m.room.message":
                                     await self.mark_message_as_read(event["event_id"])
                                 elif event["type"] == "m.room.member" and event["content"]["membership"] == "join":
-                                    # user_id = event["state_key"]
-                                    # welcome_message = f"Welcome to the room, {user_id}!"
                                     user_id = event["state_key"]
-                                    user_name = user_id.split(":")[0]  # Extract the part before the colon
-                                    welcome_message = f"Welcome to the room, {user_name}!"
+                                    welcome_message = f"Welcome to the room, {user_id}!"
+                                    # user_name = user_id.split(":")[0]  # Extract the part before the colon
+                                    # welcome_message = f"Welcome to the room, {user_name}!"
                                     await self.send_message(welcome_message)
 
 
